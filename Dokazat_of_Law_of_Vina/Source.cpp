ea83490ac73c86e3bf1b8c01aeddc2a7e923facf
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-05-31 10:59:24 +0300

    done  i hope for good

--- Dokazat_of_Law_of_Vina/Source.cpp
+++ Dokazat_of_Law_of_Vina/Source.cpp
@@ -6,17 +6,33 @@
 
 using namespace std;
 
-double R = 0;
-double R1 = 0;
+double R = 0.0;
+double R1 = 0.0;
 long double k = 1.38 * pow(10, -23);
 long double h = 6.62 * pow(10, -34);
-double c = 3 * pow(10, 8);
+double c = 3.0 * pow(10, 8);
 double sigma = 5.67 * pow(10, -8);
 double b = 2.898 * pow(10, -3);
-double lyambda = 0;
-
+double lyambda = 0.0;
+double x = 0.0;
 
 int main() {
+	for (double i = 0.0; i < 100.0; i+= 0.00001)
+	{
+		if (   ( (i * exp(i)) 
+			/ 
+			(exp(i) - 1.0) ) 
+			- 5.0 < 0.00001 && ((i * exp(i))
+				/
+				(exp(i) - 1.0))
+			- 5.0 >= 0) {
+			x = i;
+			printf("%f ", x);
+			cout << endl;
+			break;
+		}
+	}
+
 
 	ofstream File("Rezultati_vichisleniy.xls");
 
@@ -24,7 +40,7 @@ int main() {
 	for (int i = 100; i < 600; i++)
 	{
 
-		lyambda = b / i;
+		lyambda = (h * c / x / k / i);
 
 		File << lyambda <<"\t"; File << i << endl;
 
